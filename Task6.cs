f54bc2b10a1c7843de9657e575a958c463db60cb
Dachtone <dachtone.ggl@gmail.com>
2021-04-02 14:18:03 +0300

    Made the choice of N automated and incremental instead of user input in Tasks 6-8

--- Task6.cs
+++ Task6.cs
@@ -29,31 +29,20 @@ namespace NumericalAnalysis
             double a = 1.0;
             double b = 100.0;
             const double exact = 2817.8423474439187;
-            int n = GetUserIntWithMessage("n");
-
-            Console.WriteLine();
 
             Console.WriteLine("Метод прямоугольников.");
-            double approximation = RiemannSum(a, b, n);
-            Console.WriteLine($"Приближённое значение интеграла: { approximation }");
-            Console.WriteLine($"Точное значение интеграла: { exact }");
-            Console.WriteLine($"Разница приближённого и точного значений интеграла: { Math.Abs(approximation - exact) }");
+            for (int n = 10; n <= 100000; n *= 10)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine($"n = { n }");
+                double approximation = RiemannSum(a, b, n);
+                Console.WriteLine($"Приближённое значение интеграла: { approximation }");
+                Console.WriteLine($"Точное значение интеграла: { exact }");
+                Console.WriteLine($"Разница приближённого и точного значений интеграла: { Math.Abs(approximation - exact) }");
+            }
             
             Console.ReadKey();
-        }
-
-        private static int GetUserIntWithMessage(string valueName)
-        {
-            int value;
-            Console.Write($"Введите { valueName }: ");
-            if (!int.TryParse(Console.ReadLine(), out value))
-            {
-                Console.WriteLine("Ошибка ввода!");
-                Console.ReadKey();
-                Environment.Exit(13); // "The data is invalid" code
-            }
-
-            return value;
         }
     }
 }
